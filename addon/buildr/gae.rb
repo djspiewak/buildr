d81d7c4e4668121a789c406af3023d2c11441888
digitalsanctum <shane@digitalsanctum.com>
2009-08-18 15:16:11 -0700

    added datanucleus enhance task

--- addon/buildr/gae.rb
+++ addon/buildr/gae.rb
@@ -7,6 +7,10 @@ module Buildr
     HOME = ENV['GAE_HOME'] or fail 'Are we forgetting something? GAE_HOME not set.'
     LIBS = Dir["#{HOME}/lib/user/**/*.jar"]
 
+    GAE_SDK = Dir.glob(File.join(HOME, "lib", "user", "*.jar"))
+    GAE_TOOLS = File.join(HOME, 'lib', 'appengine-tools-api.jar')
+    GAE_SHARED = Dir.glob(File.join(HOME, "**", "shared", "*.jar"))
+
     class GAEConfig
       attr_reader :host, :email
       attr_writer :host, :email
@@ -25,6 +29,7 @@ module Buildr
     
     first_time do
       Project.local_task :deploy
+      Project.local_task :enhance
       Project.local_task :rollback
       Project.local_task :server
     end
@@ -38,6 +43,24 @@ module Buildr
       dev_appserver = lambda do |*args|
         trace "#{HOME}/bin/dev_appserver.sh " + args.join(' ')
         system "#{HOME}/bin/dev_appserver.sh", *args
+      end
+
+      gae_enhance = lambda do |*sources|
+
+        PROJECT_CP = project.compile.target.to_s
+
+        project.ant('enhance') do |en|
+          en.taskdef :name=>'enhance',
+                     :classname=>'com.google.appengine.tools.enhancer.EnhancerTask',
+                     :classpath=>GAE_TOOLS
+
+          en.enhance :failonerror=>true do
+            en.classpath :path=>[GAE_TOOLS, GAE_SHARED, GAE_SDK, PROJECT_CP].join(File::PATH_SEPARATOR)
+            sources.map(&:to_s).each do |source|
+              en.fileset :dir=>source.to_s, :includes=>"**/*.class"
+            end
+          end
+        end
       end
       
       war = project.package :war
@@ -55,14 +78,23 @@ module Buildr
         Dir.chdir cwd
       end
       
+      desc 'deploy' 
       task :deploy => war_dir do
         appcfg.call :update, war_dir.name
       end
+
+      desc 'enhance'
+      task :enhance, :sources do |task, args|
+        sources = args[:sources] or fail 'You must define sources to enhance!'
+        gae_enhance.call(sources)
+      end
       
+      desc 'rollback'
       task :rollback => war_dir do
         appcfg.call :rollback, war_dir.name
       end
       
+      desc 'dev_appserver'
       task :server => war_dir do
         dev_appserver.call war_dir.name
       end
